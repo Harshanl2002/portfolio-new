82aeabeb6fb62412cfa08882cbf58b24b2256e5e
harshan <harshan.loganathan@digri.ai>
2025-03-09 00:23:23 +0530

    fix: correct import path for Projects component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Experience } from "./Component/Experience";
 import Footer from "./Component/Footer";
 import HomePage from "./Component/Home";
 import Navbar from "./Component/navbar";
-import Projects from "./Component/PRoject";
+import Projects from "./Component/Project";
 import SkillsCarousel from "./Component/Skills";
 
 function App() {
